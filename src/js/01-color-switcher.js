6d137f6f0d5c58b399c908648eb12a67a397030e
Krzysztof Wiskowski <krzysztof.wiskowski@gmail.com>
2023-08-30 00:13:51 +0200

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+document.getElementById('starter').addEventListener('click', startHandler);
+document.getElementById('stopper').addEventListener('click', stopHandler);
+
+document.getElementById('stopper').disabled = true;
+
+let timerId;
+
+function startHandler() {
+  console.log('start');
+  document.body.style.backgroundColor = getRandomHexColor();
+  if (!timerId) {
+    timerId = setInterval(startHandler, 1000);
+    document.getElementById('starter').disabled = true;
+    document.getElementById('stopper').disabled = false;
+  }
+}
+
+function stopHandler() {
+  console.log('stop');
+  clearInterval(timerId);
+  timerId = null;
+  document.getElementById('starter').disabled = false;
+  document.getElementById('stopper').disabled = true;
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,16 @@
+const flatpickr = require('flatpickr');
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+const myInput = document.getElementById('datetime-picker');
+const fp = flatpickr(myInput, options);
