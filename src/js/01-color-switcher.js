6d109e8e8afa94ce05752b8deb0ac228f071e692
Krzysztof Wiskowski <krzysztof.wiskowski@gmail.com>
2023-09-05 23:25:16 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,11 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
-document.getElementById('starter').addEventListener('click', startHandler);
-document.getElementById('stopper').addEventListener('click', stopHandler);
-
-document.getElementById('stopper').disabled = true;
+const starter = document.getElementById('starter');
+starter.addEventListener('click', startHandler);
+const stopper = document.getElementById('stopper');
+stopper.addEventListener('click', stopHandler);
+stopper.disabled = true;
 
 let timerId;
 
@@ -14,8 +14,8 @@ function startHandler() {
   document.body.style.backgroundColor = getRandomHexColor();
   if (!timerId) {
     timerId = setInterval(startHandler, 1000);
-    document.getElementById('starter').disabled = true;
-    document.getElementById('stopper').disabled = false;
+    starter.disabled = true;
+    stopper.disabled = false;
   }
 }
 
@@ -23,6 +23,37 @@ function stopHandler() {
   console.log('stop');
   clearInterval(timerId);
   timerId = null;
-  document.getElementById('starter').disabled = false;
-  document.getElementById('stopper').disabled = true;
+  starter.disabled = false;
+  stopper.disabled = true;
 }
+
+// Działający kod
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// document.getElementById('starter').addEventListener('click', startHandler);
+// document.getElementById('stopper').addEventListener('click', stopHandler);
+
+// document.getElementById('stopper').disabled = true;
+
+// let timerId;
+
+// function startHandler() {
+//   console.log('start');
+//   document.body.style.backgroundColor = getRandomHexColor();
+//   if (!timerId) {
+//     timerId = setInterval(startHandler, 1000);
+//     document.getElementById('starter').disabled = true;
+//     document.getElementById('stopper').disabled = false;
+//   }
+// }
+
+// function stopHandler() {
+//   console.log('stop');
+//   clearInterval(timerId);
+//   timerId = null;
+//   document.getElementById('starter').disabled = false;
+//   document.getElementById('stopper').disabled = true;
+// }
